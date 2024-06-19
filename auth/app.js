c7b0aa6e774dcef408b8d496133178be943ad4fa
kiczyn <38925384+kiczyn@users.noreply.github.com>
2024-06-19 11:44:26 +0200

    user list and new user functionality

--- auth/app.js
+++ auth/app.js
@@ -64,8 +64,24 @@ app.post('/getData',(req,res)=>{
 })
 
 app.post('/newEmployee',(req,res)=>{
-  console.log(req.body)
+  //console.log(req.body)
   console.log("bodu")
-  console.log(req.body.employee[0].FullName)
-  return res.status(200)
+  let userData=req.body.employee;
+  console.log(JSON.stringify(userData.FullName))
+  console.log(userData)
+  console.log(JSON.stringify(userData.FullName),JSON.stringify(userData.Subdivision),JSON.stringify(userData.Position),JSON.stringify(userData.PeoplePartner),JSON.stringify(userData.OutOfOfficeBallance),JSON.stringify(userData.Login),JSON.stringify(userData.Password))
+  db.query('INSERT INTO employee ( FullName, Subdivision, Position, Status, PeoplePartner, OutOfOfficeBallance, login, pass) VALUES  (?, ?, ?,?, ?, ?, ?,?);',
+    [userData.FullName,userData.Subdivision,userData.Position,"Active",userData.PeoplePartner,userData.OutOfOfficeBallance,userData.Login,userData.Password],function(err,result){
+    if (err){
+      console.log(err)
+      return res.status(401).json({status:'err',message:'err'})
+      
+    }else{
+      console.log("succ")
+      
+      console.log(result)
+          return res.status(200).json({status:'success',message:'success'})
+    }
+  })
+  //return res.status(200)
 })
